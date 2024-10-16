df5cb9fcc37e07ea198aa5d3b7fa9ad57830e012
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-16 17:39:34 +0200

    fixing exercise 9

--- university_db.sql
+++ university_db.sql
@@ -40,6 +40,9 @@ FROM `teachers`
 WHERE `phone` IS NULL;
 
 -- 9. Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo degree_id, inserire un valore casuale)
+INSERT INTO `students` (id, degree_id, name, surname, date_of_birth, fiscal_code, enrolment_date, registration_number, email)
+VALUES (9999, 9999, "Placido", "Bonfiglio", "1997-07-31", "NPXVYW51Z02T971B", "2019-02-21", 999999, "dino@gmail.com") -- ! NON FUNZIONA
+
 
 -- 10. Cambiare il numero dell’ufficio del professor Pietro Rizzo in 126
 UPDATE `teachers`
